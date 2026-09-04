7415b4ec5442083f1a9c7bfa257b4435c9991706
Rakeshprogrammer <webprogrammerrakesh@gmail.com>
2026-09-04 16:26:03 +0530

    Solving and adding

--- Main1.java
+++ Main1.java
@@ -3,7 +3,7 @@ public class Main1{
     public static void main(String args[]){
         int number = 10;
         if(number>0){
-            System.out.println("Postive");
+            System.out.println("Positive");
         }
         else if(number<0){
             System.out.println("Negative");
---
+++ Main2.java
@@ -0,0 +1,11 @@
+//Question 2: Check if a number is even or odd.
+public class Main2{
+    public static void main(String args[]){
+        int number = 3;
+        if(number%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
