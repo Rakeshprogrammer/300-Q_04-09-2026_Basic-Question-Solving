b8f3d379cb5c3161c4e6a210b4a94489e78f8c52
Rakeshprogrammer <webprogrammerrakesh@gmail.com>
2026-09-07 18:15:54 +0530

    Logical Level 3

---
+++ LEVEL3/Main25.java
@@ -0,0 +1,12 @@
+//Question 5: Check if a number is a multiple of 7 or ends with 7.
+
+public class Main25{
+    public static void main(String args[]){
+        int number = 7007;
+        if(number%7==0 || number%10==7){
+            System.out.println("number is a multiple of 7 or ends with 7.");
+        }else{
+              System.out.println("number is not a  multiple of 7 or ends with 7.");
+    }
+}
+}
